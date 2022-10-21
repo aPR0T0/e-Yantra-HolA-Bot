0105bf880044aa2f9020cd8e0da41252dc4c31e1
aPR0T0 <awshaikh_b21@me.vjti.ac.in>
2022-10-21 19:37:36 +0530

    Tuned according to the requirements

--- task_one/controller.py
+++ task_one/controller.py
@@ -17,7 +17,7 @@ import math
 import numpy as np
 
 # Getting the float64MultiArray for subscription
-from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Float64MultiArray, Float64
 # Odometry is given as a quaternion, but for the controller we'll need to find the orientaion 0 by converting to euler angle
 from tf.transformations import euler_from_quaternion
 
@@ -29,7 +29,8 @@ vel_x = 0
 vel_y = 0
 vel_z = 0
 Helper_time = 0
-index = 0		# this will help us determine what element are we currently on
+index = 0
+flag = 0
 
 # Initialise variables that may be needed for the control loop
 # For ex: x_d, y_d, theta_d (in **meters** and **radians**) for defining desired goal-pose.
@@ -38,23 +39,24 @@ des_y = 0
 des_theta = 0
 
 # desired coordinates but in the form of the arrays
-x_goals     = [0,0,0,0,0]
-y_goals     = [0,0,0,0,0]
-theta_goals = [0,0,0,0,0]
-
+x_goals           = [0,0,0,0,0]
+y_goals           = [0,0,0,0,0]
+theta_goals       = [0,0,0,0,0]    
+prev_x_goals	  = [0,0,0,0,0]
+prev_y_goals      = [0,0,0,0,0]
+prev_theta_goals  = [0,0,0,0,0]
 # and also Kp values for the P Controller
 kp_x = 2
 kp_y = 2
 kp_theta = 5 #initializing kp
 
-prev_x_goals, prev_y_goals, prev_theta_goals = [0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]
 #Taking input from the user for the desired co-ordinates
 # des_x, des_y, des_theta = map(float, input("Specify your desired x and y coordinate and also specify the orientation:").split())
 
 # Taking input from a topic known as auto-eval
 def task1_goals_Cb(msg):
 	global x_goals, y_goals, theta_goals, prev_x_goals, prev_y_goals, prev_theta_goals
-
+    
 	x_goals.clear()
 	y_goals.clear()
 	theta_goals.clear()
@@ -67,12 +69,12 @@ def task1_goals_Cb(msg):
 		orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
 		theta_goal = euler_from_quaternion (orientation_list)[2]
 		theta_goals.append(theta_goal)
-	print(x_goals, y_goals, theta_goals)
-	if(x_goals == [] and y_goals == [] and theta_goals == []):
-		x_goals     = [0,0,0,0,0]
-		y_goals     = [0,0,0,0,0]
-		theta_goals = [0,0,0,0,0]
+
+	if(len(x_goals) == 0 or len(y_goals) == 0 or len(theta_goals) == 0):
 		rospy.Subscriber('task1_goals', PoseArray, task1_goals_Cb)	
+	else:
+		prev_x_goals, prev_y_goals, prev_theta_goals = x_goals, y_goals, theta_goals
+		# des_x, des_y, des_theta =  x_goals[0], y_goals[0], theta_goals[0]
 
 # Function to recieve values from the user
 def SetValue(msg):
@@ -95,13 +97,23 @@ def odometryCb(msg):
 	# Write your code to take the msg and update the three variables
 
 def main():
-		global pub, vel_x, vel_y,vel_z, kp_x, kp_y, kp_theta, rotation_matrix, current_time, Helper_time, index, des_x, des_y, des_theta
+		global pub, vel_x, vel_y,vel_z, kp_x, kp_y, kp_theta, rotation_matrix, current_time, Helper_time, index, des_x, des_y, des_theta, flag
 		# Initialze Node
 		# We'll leave this for you to figure out the syntax for 
 		# initialising node named "controller"
 		rospy.init_node('controller_node', anonymous=False)
 		# Initialze Publisher and Subscriber
+
 		pub = rospy.Publisher("/cmd_vel", Twist, queue_size=100, latch=True)
+		
+		# err_x_pub = rospy.Publisher("/err_x", Float64, queue_size=100)
+		
+		# err_y_pub = rospy.Publisher("/err_y", Float64, queue_size=100)
+		
+		# err_theta_pub = rospy.Publisher("/err_theta", Float64, queue_size=100)
+		
+		# des_x, des_y, des_theta = x_goals[0], y_goals[0], theta_goals[0]
+		# rospy.Subscriber('P_val', Float64MultiArray, SetValue)
 		# We'll leave this for you to figure out the syntax for
 		# initialising publisher and subscriber of cmd_vel and odom respectively
 		# Declare a Twist message
@@ -109,29 +121,26 @@ def main():
 		# Initialise the required variables to 0
 		# <This is explained below>
 		
-		# x_goals = [1,-1,-1,1,0] 
-		# y_goals = [1,1,-1,-1,0]
-		# theta_goals = [0.785, 2.335, -2.335, -0.785, 0]
 		# For maintaining control loop rate.
 		rate = rospy.Rate(100)
 
-		# x_goals = [1, -1, -1, 1, 0]
-		# y_goals = [1, 1, -1, -1, 0]
-		# theta_goals = [math.pi, 3*math.pi/4, -3*math.pi/4, -math.pi/4, 0]
+		# x_goals = [1,0,0,0,1]
+		# y_goals = [0,1,0,0,0]
+		# theta_goals = [0, 1.54, 1, 3, -3]
 		#
 		# For tuning, accepting p_values from the message publisher 
-		rospy.Subscriber("P_val", Float64MultiArray, SetValue)
+		
 		#
 		#
 		# # declare that the node subscribes to task1_goals along with the other declarations of publishing and subscribing
-		# rospy.Subscriber('task1_goals', PoseArray, task1_goals_Cb)
+		rospy.Subscriber('task1_goals', PoseArray, task1_goals_Cb)
 			
 		
+			
 		# declare that the node subscribes to task1_goals along with the other declarations of publishing and subscribing
 
 		while not rospy.is_shutdown():
 			rospy.Subscriber("/odom", Odometry, odometryCb)
-			rospy.Subscriber('task1_goals', PoseArray, task1_goals_Cb)	
 			# Getting time for the arrays
 			current_time = time.time()
 			sample_time  = 	   2
@@ -139,8 +148,10 @@ def main():
 			# the /odom topic is giving pose of the robot in global frame
 			# the desired pose is declared above and defined by you in global frame
 			# therefore calculate error in global frame
+			# rospy.Subscriber('task1_goals', PoseArray, task1_goals_Cb)	
+			# # Taking input for about 1s 
+			# rospy.Subscriber('task1_goals', PoseArray, task1_goals_Cb)
 			
-			# Taking input for about 1s 
 			if (des_x - 0.01 <= hola_x <= 0.01 + des_x and des_y - 0.01 <= hola_y <= des_y + 0.01 and  des_theta - 0.005 <= hola_theta <= des_theta + 0.005):
 				if( current_time - Helper_time >= sample_time ):
 					if( 0 <= index < len(x_goals) and 0 <= index < len(y_goals) and 0 <= index < len(theta_goals)):
@@ -148,17 +159,29 @@ def main():
 						des_y = y_goals[index]
 						des_theta = theta_goals[index]
 						index += 1
-					else:
 						if(index == len(x_goals)):
-							rospy.Subscriber('task1_goals', PoseArray, task1_goals_Cb)	
-							index = 0
+							if(index == 0):
+								rospy.Subscriber('task1_goals', PoseArray, task1_goals_Cb)
+							else: 
+								index = 0
+								rospy.sleep(5)
 			else:
 				Helper_time = current_time
 
-			err_x = des_x - hola_x
+
+			err_x = des_x - hola_x 
 			err_y = des_y - hola_y
 			err_z = 0
 			err_theta = des_theta - hola_theta
+
+			# Publishing errors in order to identify the problem by plotting on the rqt
+
+			# err_x_pub.publish(err_x)
+			
+			# err_y_pub.publish(err_y)
+			
+			# err_theta_pub.publish(err_theta)
+
 			rotation_matrix = np.array([[math.cos(0)*math.cos(hola_theta),math.sin(hola_theta)*math.cos(0),-math.sin(0)],
 										[math.sin(0)*math.sin(0)*math.cos(hola_theta)-math.cos(0)*math.sin(hola_theta),
 										math.sin(0)*math.sin(0)*math.sin(hola_theta)+math.cos(0)*math.cos(hola_theta),
